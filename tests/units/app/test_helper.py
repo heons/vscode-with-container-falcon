c4bf6a590aaec69cd12e2ff345dc28052763203c
HUEN OH <huen.oh@ssense.com>
2021-11-16 13:47:38 -0500

    add test_helper.py

---
+++ tests/units/app/test_helper.py
@@ -0,0 +1,13 @@
+import unittest
+from app.helper import helper_func1
+
+class TestHelper(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_helper_func1(self):
+        str_compare = "helper_fun1"
+        result = helper_func1()
+
+        self.assertEqual(str_compare, result)
+        
